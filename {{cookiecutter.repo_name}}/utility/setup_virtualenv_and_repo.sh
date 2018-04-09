86a6429788db7a78433d75c6b4a32547a9744ac5
jackdbd <jackdebidda@gmail.com>
2018-04-09 20:16:12 +0200

    Add utility for generated project

---
+++ {{cookiecutter.repo_name}}/utility/setup_virtualenv_and_repo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkvirtualenv {{cookiecutter.repo_name}} --python=python3.6
+pip install -r requirements.txt
+git init
+git add .
+git commit -m "Initial commit"
+
+echo "Here is your project tree:"
+tree -L 2
