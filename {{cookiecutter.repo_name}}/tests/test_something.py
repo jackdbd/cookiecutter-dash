6bf1aec4499692b3a0625f86d341cfbc8d580176
jackdbd <jackdebidda@gmail.com>
2018-04-09 20:16:31 +0200

    Add tests for generated project

---
+++ {{cookiecutter.repo_name}}/tests/test_something.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestYourTestClass(unittest.TestCase):
+
+    def test_your_test_case(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
